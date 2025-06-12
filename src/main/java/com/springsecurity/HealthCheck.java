881ec79695796330e12c1aceede5110d2ee4b035
Prathameshghorapade <pghorapade17@gmail.com>
2025-06-12 20:02:44 +0530

    spring security Basic

---
+++ src/main/java/com/springsecurity/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String SayHello(){
+        return "Everything Ok";
+    }
+}
