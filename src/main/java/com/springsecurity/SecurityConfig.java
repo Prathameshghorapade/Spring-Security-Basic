c47cf77a85d5d35a0f1fe3e4765a94dc92e4930f
Prathameshghorapade <pghorapade17@gmail.com>
2025-06-12 22:26:16 +0530

    complete In Memory Authentication

---
+++ src/main/java/com/springsecurity/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}user1")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}user2")
+                .roles("USER")
+                .build();
+
+      return new InMemoryUserDetailsManager(user1,user2);
+
+    }
+
+
+}
